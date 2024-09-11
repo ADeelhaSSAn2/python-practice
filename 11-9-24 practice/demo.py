56fa70c94935fe3887295f7811dc9fc09dd68cc6
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-12 00:29:32 +0500

    update

--- 11-9-24 practice/demo.py
+++ 11-9-24 practice/demo.py
@@ -1,6 +1,40 @@
-print("Hellow world")
- name = "adeel"
- print(name)
+# print("Hellow world")
+#  name = "adeel"
+#  print(name)
  
 
+# name = input("Enter Your Name")
+# print("hi" , name)
 
+# import matplot.pyplot as plt
+# plt.plot([1,2,3,4],[1,2,3,4])
+# plt.show()
+
+# integers
+x = 100
+type(x)
+print(x)
+
+# float
+y = 2.84
+print(y)
+type(y)
+  
+#   string
+z = "adeel"
+print(z)
+  
+#   list
+x= [13,23,22]
+print(x[0])
+x[1]=55
+print(x)
+
+# tuple
+u =(12,33,55)
+print(u)
+
+a =2
+b =3
+result = a+b
+print(result)
