570367e98f9c566082b20574870747c490e4e6fe
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-12 23:10:22 +0500

    update

---
+++ 12-9-24 youtube se practice/a.py
@@ -0,0 +1,68 @@
+# data type in python
+
+# integers
+a = 23
+print(type(a))
+b = 23444
+print(type(b))
+
+# float
+num = 2.53
+print(type(num))
+val = 234.483
+print(type(val))
+
+# complex numbers
+
+comp = 2+5j
+print(type(comp))
+print(comp.real)
+print(comp.imag)
+
+# Arithmetic operation
+num1 = 24
+num2 = 26
+print(num1 + num2)
+
+num3 = 23
+num4 =3
+print(num3 - num4)
+
+num5 = 44
+num6 = 45
+print (num5 * num6)
+
+num7 =43
+num8 = 7
+print(num7 / num8)
+
+num9 = 51
+num0 =12
+print(num9 // num0)
+
+numA = 2
+numB =10
+print(numA % numB)
+
+
+nuM = 5
+nUm = 10
+print(nuM ** nUm)
+
+# string
+
+st = "192"
+print(type(st))
+print(st)
+
+st = int(st)
+print(type(st))
+print(st)
+
+st = float(st)
+print(type(st))
+print(st)
+
+st = complex(st)
+print(type(st))
+print(st)
