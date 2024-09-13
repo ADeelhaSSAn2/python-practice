bba495e7beb4b2a56f122de33a40b9be023851a1
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-14 00:19:36 +0500

    update

---
+++ 13-9-24 lesson #3/code.py
@@ -0,0 +1,81 @@
+# 
+# String in python
+# 
+sti ="Asslam O Alikum"
+print(sti)
+
+srt= 'Asslam O Alikum'
+print(srt)
+
+st='Adeel said "I am busy today"'
+print(st)
+
+s ='Adeel said "I\'m busy today"'
+print(s)
+t='''Adeel said 
+I am busy today'''
+print(t)
+
+ln="adeelhassan"
+print(len(ln))
+print(ln[5])
+print(ln[0:5])
+print(ln[5:])
+print(ln[:5])
+
+up="welcome to house"
+print(up.upper())
+print(up.find('o'))
+print(up.replace("house", "python world"))
+low="WELCOME"
+print(low.lower())
+print(low.split(" "))
+
+stg1 ="good"
+stg2 ="night"
+stg3 ="boy"
+stg= stg1 + stg2 +stg3
+print(stg)
+
+stg1 ="good"
+stg2 ="night"
+stg3 ="boy"
+stg= "{} {} {}".format(stg1,stg2,stg3)
+print(stg)
+
+# 
+# creating a dictionaries
+# 
+d1 ={}
+print(d1)
+print(type(d1))
+
+d2={1:"Welcome", 2:"to", 3:"python"}
+print(d2)
+
+d3 ={
+    "name":"Adeel",
+    "age":"22",
+    "profession":"student"
+     }
+print(d3)
+d4 =({1:"Welcome", 2:"to", 3:"python"})
+print(d4)
+d4 =[{1:"Welcome", 2:"to", 3:"python"}]
+print(d4)
+
+d5={0:"we",1:"are",2:"one"}
+print(d5.pop(0))
+print(d5)
+
+# 
+# set
+# 
+s =set([1,2,3,4])
+print(s)
+print(type(s))
+
+s1 =set([1,3,7,2])
+s2 =set([3,2,8,9])
+print(s1.union(s2))
+print(s1.difference(s2))
