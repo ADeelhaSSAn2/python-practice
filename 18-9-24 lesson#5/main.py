829da18151f85a7ae025ff6f39b1b87191a4162a
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-18 16:37:40 +0500

    up

--- 18-9-24 lesson#5/main.py
+++ 18-9-24 lesson#5/main.py
@@ -11,3 +11,9 @@ for i in a :
     
 for i in range(0,11):
     print(i)
+    
+n = int(input("enter a number"))
+for i in range(1, n+1):
+    for j in range(1, i+1): 
+        print(j , end='')
+        print()
