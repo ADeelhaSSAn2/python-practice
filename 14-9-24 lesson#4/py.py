a66fd9b60d6b11df9579a9d9bf972efa40b39d1b
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-18 15:36:45 +0500

    update

---
+++ 14-9-24 lesson#4/py.py
@@ -0,0 +1,36 @@
+# Conditional statement
+
+# if statement
+
+# a = 60
+# if a>50:
+#     print("This is if body")
+#     print("This is outside the if body")
+    
+   
+# c=21
+# if c>21:
+#     if c%2==0:
+#         print("c is an even number less than 25")
+#         else:
+#             print("c is an odd number less than 25")
+#             else:
+#                 print("c is greater than 25")        
+    
+        
+# var ="z"
+# if var=='a':
+#     print("This is the vowel a")
+# elif var =='e':
+#     print("This is the vowel e")
+#  elif var =='i':
+#     print("This is the vowel i")
+# elif var =='o':
+#     print("This is the vowel o")   
+# elif var =='u':
+#     print("This is the vowel u")
+#     else:
+#         ("This is constant")
+    
+    
+    
