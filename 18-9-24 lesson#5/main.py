9e5a31a5ce5442dcdfd3f39c583e1e3c3302057a
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-18 23:24:01 +0500

    update

--- 18-9-24 lesson#5/main.py
+++ 18-9-24 lesson#5/main.py
@@ -1,19 +1,66 @@
 # for loop
 
-x = "adeelhassan"
-for i in x :
-    print(i)
+# x = "adeelhassan"
+# for i in x :
+#     print(i)
     
 
-a = "adeelhassan"
-for i in a :
-    print(i, end='')
+# a = "adeelhassan"
+# for i in a :
+#     print(i, end='')
     
-for i in range(0,11):
-    print(i)
+# for i in range(0,11):
+#     print(i)
     
-n = int(input("enter a number"))
-for i in range(1, n+1):
-    for j in range(1, i+1): 
-        print(j , end='')
-        print()
+# n = int(input("enter a number"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1): 
+#         print(j , end='')
+#         print()
+        
+# x =[1,4.2,"Adeel"]
+# for i in x:
+#     print(i)
+
+# print(x[0],x[1],x[2])       
+
+# a ="adeel"
+# for i in a:
+#     print(i , end='')
+    
+# n =int(input("enter a number"))
+# for i in range(1,n+1):
+#   for j in range(1,i+1):
+#     print(j , end='')
+#     print()    
+        
+# n =int(input("enter a number"))
+# i = 1    
+# while i<=n:
+#     j =1
+#     while j<=i :
+#         print(i , end='')
+#         j+=1
+#         i +=1
+#         print()
+
+
+colors = ["red","blue","green"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+        # 
+ 
+        
+
+for k in range(10):
+    print(k +1)
+    
+
+
+
+
+
+
+
