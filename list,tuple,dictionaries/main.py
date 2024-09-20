9d4773d967f2b5d41e3aa78e9320245635d30fc5
Adeel Hassan <adeelhassan35033@gmail.com>
2024-09-20 23:25:20 +0500

    update

---
+++ list,tuple,dictionaries/main.py
@@ -0,0 +1,44 @@
+# list in python
+
+marks =[94.5, 88.2, 92.1, 76.9]
+print(marks)
+print(len(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2:4])
+
+data = ["Adeel", 99.9, 22, "Karachi"]
+print(data)
+
+data[0] = "Taqi"
+print(data)
+
+# last mein value add krega
+lis = [1,2,3]
+lis.append(4)
+print(lis)
+
+# value ko sequence wise likhyga
+li = [1,3,6,9,8,4,2]
+li.sort()
+print(li)
+
+# value ko reverce krdega
+l = ["a", "b", "c", "d"]
+l.reverse()
+print(l)
+
+# value middle mein or kahi se bhi add kr sukhty hai
+i = [1,3,4]
+i.insert(1,5)
+print(i)
+
+# value remove kr sukhty hai
+p = [1,2,3,4,5]
+p.pop(1)
+print(p)
+
+# tuples in python
+tu = (1,2,3,4)
+print(type(tu))
+
